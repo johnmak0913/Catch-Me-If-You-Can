b217887e9e8c811343050d7841876a877e671ac8
John Mak <u3556338@connect.hku.hk>
2021-03-13 00:56:50 +0800

    Teacher Walk

---
+++ 3329GroupProject/Assets/Scripts/TeacherWalk.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacherWalk : MonoBehaviour
+{
+    float speed = 1f;
+    Vector3 rightBoundary = new Vector3(4.91f, 1.06f, 0);
+    Vector3 leftBoundary = new Vector3(0.44f, 3.59f, 0);
+    Vector3 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = rightBoundary;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if(transform.position == rightBoundary)
+        {
+            targetPosition = leftBoundary;
+        }
+        else if (transform.position == leftBoundary)
+        {
+            targetPosition = rightBoundary;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+    }
+}
