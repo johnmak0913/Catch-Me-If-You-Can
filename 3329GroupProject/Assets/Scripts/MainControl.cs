bfc4c2e5a0b1eb53473cfa3a4c8a27e1f6106310
williamchankwokhei <williamchankwokhei@gmail.com>
2021-03-25 17:11:19 +0800

    Changed timer system. Added teacher random turning around.

--- 3329GroupProject/Assets/Scripts/MainControl.cs
+++ 3329GroupProject/Assets/Scripts/MainControl.cs
@@ -7,27 +7,63 @@ public class MainControl : MonoBehaviour
 {
     private Action eating, sleeping;
     public int marks=0;
-    public int timeLeft=60;
+    public float timeLeft=60.0f;
     public float turnAroundDelay=1.0f;
+    public float tTurnTimeLeft=-10.0f, tBackTimeLeft=-10.0f;
+    public bool tTurnWaiting=false, tBackWaiting=false;
     public TeacherMovement teacher;
 
-    void startTimer(int timeLimit) {
-        timeLeft=timeLimit;
-        InvokeRepeating("countdown", 0.1f, timeLimit);
-    }
-    void countdown() {
-        timeLeft--;
+    bool updateTimer() {
+        timeLeft-=Time.deltaTime;
+        return timeLeft>=0;
     }
     void teacherTurnAround() {
         teacher.preTurnAround=true;
         teacher.animator.SetBool("preTurnAround", true);
         teacher.Invoke("turnAround", turnAroundDelay);
     }
+    void teacherTurnBack() {
+        teacher.animator.SetBool("turnAround", false);
+        teacher.animator.SetBool("caught", false);
+        teacher.preTurnAround=false;
+    }
 
     public bool teacherTurnedAround() {
         return teacher.animator.GetBool("turnAround");
     }
-
+    public bool teacherIsWalking() {
+        return teacher.animator.GetCurrentAnimatorStateInfo(0).IsName("OldTeacher_Walk");
+    }
+    void teacherRandomlyTurnAround() {
+        if(teacherIsWalking()) {
+            if(!tTurnWaiting) {
+                tTurnWaiting=true;
+                tBackWaiting=false;
+                tTurnTimeLeft=UnityEngine.Random.Range(7.0f, 10.0f);
+                return;
+            }
+            if(tTurnTimeLeft<=0) {
+                teacherTurnAround();
+            }
+            else {
+                tTurnTimeLeft-=Time.deltaTime;
+            }
+        }
+        else if(teacherTurnedAround()) {
+            if(!tBackWaiting) {
+                tBackWaiting=true;
+                tBackTimeLeft=UnityEngine.Random.Range(3.0f, 5.0f);
+                return;
+            }
+            if(tBackTimeLeft<=0f) {
+                teacherTurnBack();
+                tTurnWaiting=false;
+            }
+            else {
+                tBackTimeLeft-=Time.deltaTime;
+            }
+        }
+    }
     public void getCaught() {
         teacher.animator.SetBool("caught", true);
     }
@@ -37,23 +73,22 @@ public class MainControl : MonoBehaviour
         teacher=GameObject.Find("OldTeacher").GetComponent<TeacherMovement>();
         eating=new Action(KeyCode.E, 3, 3, 1);
         sleeping=new Action(KeyCode.Z, 5, 2, 2);
-        startTimer(60);
+        timeLeft=60.0f;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if(Input.GetKey(KeyCode.T)) {
-            teacherTurnAround();
-        }
-        if(Input.GetKey(KeyCode.R)) {
-            teacher.animator.SetBool("turnAround", false);
-            teacher.animator.SetBool("caught", false);
-            teacher.preTurnAround=false;
-        }
-        if(timeLeft<=0) {
+        teacherRandomlyTurnAround();
+        if(!updateTimer()) {
             Debug.Log("Time's up");
             return;
+        }
+        if(Input.GetKey(KeyCode.R)) {
+            teacherTurnBack();
+        }
+        if(Input.GetKey(KeyCode.T)) {
+            teacherTurnAround();
         }
         if(eating.check())
             return;
